0af517f6ad63bf2a94ae0240271edced51bb33c4
Zero <54469706+3JoB@users.noreply.github.com>
2023-04-08 17:16:25 +0800

    add

---
+++ internal/service/gallery/process.go
@@ -0,0 +1,5 @@
+package gallery
+
+// This service is used to process pictures in the gallery, 
+// including: image resolution processing, image review, 
+// image transcoding, thumbnail generation and other services.
